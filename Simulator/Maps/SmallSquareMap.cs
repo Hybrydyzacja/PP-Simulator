f103996938033f0261204d87d89fb69ea5c1d4e4
Ola <ziebaaleksandra97@gmail.com>
2025-01-05 16:22:23 +0100

    Mapy

---
+++ Simulator/Maps/SmallSquareMap.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Simulator.Maps;
+
+public class SmallSquareMap : Map
+{
+    public readonly int Size;
+    private readonly Rectangle bounds;
+
+    public SmallSquareMap(int size)
+    {
+        if (size < 5 || size > 20)
+        {
+            throw new ArgumentOutOfRangeException(nameof(size), "Nieprawidłowy rozmiar, dopuszczalne wartości boku <5;20>");
+        }
+        Size = size;
+        bounds = new Rectangle(0, 0, size - 1, size - 1);
+    }
+    
+    public override bool Exist(Point p)
+    {
+        return bounds.Contains(p);
+    }
+
+    public override Point Next(Point p, Direction d)
+    {
+        Point nextPoint = p.Next(d);
+        return Exist(nextPoint) ? nextPoint : p;
+    }
+
+    public override Point NextDiagonal(Point p, Direction d)
+    {
+        Point nextDiagonalPoint = p.NextDiagonal(d);
+        return Exist(nextDiagonalPoint) ? nextDiagonalPoint : p;
+    }
+}
--- Simulator/Simulator.cs
+++ Simulator/Simulator.cs
@@ -1,4 +1,6 @@
-﻿namespace Simulator;
+﻿using Simulator.Maps;
+
+namespace Simulator;
 
 public class Simulator
 {
@@ -11,52 +13,90 @@ public class Simulator
 
 
         {
-            Lab5a();
+            //Lab5a();
+            Lab5b();
         }
 
-        static void Lab5a()
+        //static void Lab5a()
+        //{
+        //    try
+        //    {
+        //        Console.WriteLine("Test 1: Tworzenie prostokąta ze współrzędnych");
+        //        Rectangle rectangle1 = new Rectangle(2, 3, 5, 7);
+        //        Console.WriteLine($"Stworzono prostokąt: {rectangle1}");
+        //        Console.WriteLine($"Czy punkt (3, 4) znajduje się w prostokącie? {rectangle1.Contains(new Point(3, 4))}");  // True
+        //        Console.WriteLine($"Czy punkt (6, 8) znajduje się w prostokącie? {rectangle1.Contains(new Point(6, 8))}");  // False
+
+
+        //        Console.WriteLine("\nTest 2: Tworzenie prostokąta z punktów");
+        //        Point p1 = new Point(1, 1);
+        //        Point p2 = new Point(6, 6);
+        //        Rectangle rectangle2 = new Rectangle(p1, p2);
+        //        Console.WriteLine($"Stworzono prostokąt: {rectangle2}");
+        //        Console.WriteLine($"Czy punkt (3, 3) znajduje się w prostokącie? {rectangle2.Contains(new Point(3, 3))}");  // True
+        //        Console.WriteLine($"Czy punkt (7, 7) znajduje się w prostokącie? {rectangle2.Contains(new Point(7, 7))}");  // False
+
+
+        //        Console.WriteLine("\nTest 3: Tworzenie prostokąta z uporządkowanymi wspołrzędnymi");
+        //        Rectangle rectangle3 = new Rectangle(7, 6, 2, 3);  // Konieczna zmiana kolejnosci punktów
+        //        Console.WriteLine($"Stworzono prostokąt z zamienionymi współrzędnymi: {rectangle3}");  // Oczekiwane: (2, 3):(7, 6)
+        //        Console.WriteLine($"Czy punkt (3, 4) znajduje się w prostokącie? {rectangle3.Contains(new Point(3, 4))}");  // True
+        //        Console.WriteLine($"Czy punkt (8, 8) znajduje się w prostokącie? {rectangle3.Contains(new Point(8, 8))}");  // False
+
+
+        //        Console.WriteLine("\nTest 4: Próba stworzenia \"chudego\" prostokąta");
+        //        try
+        //        {
+        //            Rectangle invalidRect1 = new Rectangle(5, 5, 5, 10);  // Błąd
+        //        }
+        //        catch (ArgumentException ex)
+        //        {
+        //            Console.WriteLine($"{ex.Message}");  // Oczekiwany wyjątek
+        //        }
+
+                
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine($"{ex.Message}");
+        //    }
+        //}
+
+        static void Lab5b()
         {
             try
             {
-                Console.WriteLine("Test 1: Tworzenie prostokąta ze współrzędnych");
-                Rectangle rectangle1 = new Rectangle(2, 3, 5, 7);
-                Console.WriteLine($"Stworzono prostokąt: {rectangle1}");
-                Console.WriteLine($"Czy punkt (3, 4) znajduje się w prostokącie? {rectangle1.Contains(new Point(3, 4))}");  // True
-                Console.WriteLine($"Czy punkt (6, 8) znajduje się w prostokącie? {rectangle1.Contains(new Point(6, 8))}");  // False
+                var map = new SmallSquareMap(10);
+                Console.WriteLine($"Map size: {map.Size}");
 
+                var point1 = new Point(5, 5);
+                var point2 = new Point(5, 10);
+                var point3 = new Point(10, 5);
+                var point4 = new Point(10, 10);
+                var point5 = new Point(1, 1);
+                Console.WriteLine($"Point {point1} is in the map: {map.Exist(point1)}");
+                Console.WriteLine($"Point {point2} is in the map: {map.Exist(point2)}");
+                Console.WriteLine($"Point {point3} is in the map: {map.Exist(point3)}");
+                Console.WriteLine($"Point {point4} is in the map: {map.Exist(point4)}");
+                Console.WriteLine($"Point {point5} is in the map: {map.Exist(point5)}");
 
-                Console.WriteLine("\nTest 2: Tworzenie prostokąta z punktów");
-                Point p1 = new Point(1, 1);
-                Point p2 = new Point(6, 6);
-                Rectangle rectangle2 = new Rectangle(p1, p2);
-                Console.WriteLine($"Stworzono prostokąt: {rectangle2}");
-                Console.WriteLine($"Czy punkt (3, 3) znajduje się w prostokącie? {rectangle2.Contains(new Point(3, 3))}");  // True
-                Console.WriteLine($"Czy punkt (7, 7) znajduje się w prostokącie? {rectangle2.Contains(new Point(7, 7))}");  // False
+                var startPoint = new Point(6, 5);
+                var nextPoint = map.Next(startPoint, Direction.Left);
+                Console.WriteLine($"Start point: {startPoint}, Next point: {nextPoint}");
+                var nextPoint2 = map.Next(startPoint, Direction.Right);
+                Console.WriteLine($"Start point: {startPoint}, Next point: {nextPoint2}");
+                var nextPoint3 = map.Next(startPoint, Direction.Up);
+                Console.WriteLine($"Start point: {startPoint}, Next point: {nextPoint3}");
 
-
-                Console.WriteLine("\nTest 3: Tworzenie prostokąta z uporządkowanymi wspołrzędnymi");
-                Rectangle rectangle3 = new Rectangle(7, 6, 2, 3);  // Konieczna zmiana kolejnosci punktów
-                Console.WriteLine($"Stworzono prostokąt z zamienionymi współrzędnymi: {rectangle3}");  // Oczekiwane: (2, 3):(7, 6)
-                Console.WriteLine($"Czy punkt (3, 4) znajduje się w prostokącie? {rectangle3.Contains(new Point(3, 4))}");  // True
-                Console.WriteLine($"Czy punkt (8, 8) znajduje się w prostokącie? {rectangle3.Contains(new Point(8, 8))}");  // False
-
-
-                Console.WriteLine("\nTest 4: Próba stworzenia \"chudego\" prostokąta");
-                try
-                {
-                    Rectangle invalidRect1 = new Rectangle(5, 5, 5, 10);  // Błąd
-                }
-                catch (ArgumentException ex)
-                {
-                    Console.WriteLine($"{ex.Message}");  // Oczekiwany wyjątek
-                }
-
-                
+                var diagonalPoint = map.NextDiagonal(startPoint, Direction.Up);
+                Console.WriteLine($"Diagonal point: {diagonalPoint}");
             }
-            catch (Exception ex)
+
+            catch (ArgumentOutOfRangeException ex)
             {
-                Console.WriteLine($"{ex.Message}");
+                Console.WriteLine($"Error: {ex.Message}");
             }
         }
+        
     }
 }
